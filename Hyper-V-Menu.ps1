88088830c51f7766522136e6071d53e9bb399d05
PCavar <you@example.com>
2022-12-19 11:47:05 +0100

    COMIT

--- Hyper-V-Menu.ps1
+++ Hyper-V-Menu.ps1
@@ -195,8 +195,8 @@ Invoke-Command -VMName $VMName -Credential $VMName\Administrator -ScriptBlock {
     -Credential (Get-Credential POWERSHELL\Administrator) `
     -CriticalReplicationOnly:$false `
     -SiteName "Default-First-Site-Name" `
-    -DomainName "$Using:domainNameReplicationVM" `
-    -ReplicationSourceDC $Using:ADReplicationSourceName`
+    -DomainName "POWERSHELL.LOCAL" `
+    -ReplicationSourceDC "AD01.POWERSHELL.LOCAL"`
     -DatabasePath "C:\Windows\NTDS" `
     -InstallDns:$true `
     -LogPath "C:\Windows\NTDS" `
@@ -421,8 +421,6 @@ do {
                     } '4' {
                     Get-VM | Select-Object Name,State,CPUUsage,Version | Format-Table
                     $VMName = Read-Host "Enter DC to join Domain"
-                    $domainNameReplicationVM = Read-Host "Enter Domainname EX Powershell.local"
-                    $ADReplicationSourceName = Read-Host "Enter replication Source EX AD01.domainname.local"
                     if(Get-VM -Name $VMName) {
                     New-AddDCToExistingDomain
                     } else { 
